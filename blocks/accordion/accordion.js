b62ad906f3a6aad610132b8c625aebc8d4d9d34f
Jan Lemmens <>
2025-05-05 14:31:53 +0200

    Accordion update

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,23 +1,23 @@
-import { replaceElementTagName } from '../../scripts/utils.js';
+/*
+ * Accordion Block
+ * Recreate an accordion
+ * https://www.hlx.live/developer/block-collection/accordion
+ */
 
 export default function decorate(block) {
-  [...block.children].forEach((accordionItem) => {
-    accordionItem.classList.add('accordion-item');
-
-    [...accordionItem.children].forEach((item, index) => {
-      if (index === 0) {
-        replaceElementTagName(item, 'button', ['accordion-item__trigger']);
-      } else {
-        item.classList.add('accordion-item__content');
-      }
-    });
-  });
-
-  const accordionTriggers = block.querySelectorAll('.accordion-item__trigger');
-
-  [...accordionTriggers].forEach((trigger) => {
-    trigger.addEventListener('click', () => {
-      trigger.parentElement.classList.toggle('accordion-item--active');
-    });
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...label.childNodes);
+    // decorate accordion item body
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
+    // decorate accordion item
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, body);
+    row.replaceWith(details);
   });
 }
